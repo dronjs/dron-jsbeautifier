58381a2a1b91001496edf5fcf74175c9606b5758
Vladimir Morulus <vladimirmorulus@gmail.com>
2016-09-27 19:47:55 +0300

    initial commiy

---
+++ README.md
@@ -0,0 +1,36 @@
+Beautify javascript file
+--
+
+# Install
+```
+npm install dron-jsbeautifier -g
+```
+
+# Usage from CLI
+
+For fast beautify file:
+```
+dron jsbeautifier index.js
+```
+
+The module uses a library `js-beautifier`. Read js-beautifier [documentation](https://www.npmjs.com/package/js-beautify) for advanced CLI options.
+
+# Usage from API
+
+You can use `dron-jsbeautifier` module from another Dron module like that:
+
+```js
+function createAndBeautifyFile(content) {
+	return function() {
+		this.touch('index.js').write(content);
+		return this.run('jsbeautifier', {
+			file: this.touch('index.js').fullname
+		}).then(function() {
+			return true;
+		});
+	}
+}
+```
+
+# Author
+Vladimir Kalmykov <vladimirmorulus@gmail.com>
---
+++ index.js
@@ -0,0 +1,52 @@
+var beautify = require('js-beautify').js_beautify;
+
+/**
+ * Convert dashed object keys to bottom dashes
+ * @return {[type]} [description]
+ */
+function keysToBottomDashes(object) {
+	var bd = {};
+	for (var prop in object) {
+		if (object.hasOwnProperty(prop)) bd[prop.replace('-', '_')] = object[prop];
+	}
+	return bd;
+}
+
+/**
+ Dron module `dron-jsbeautifier`*
+*/
+function dronJsbeautifier(file, options) {
+	return function() {
+		var content = beautify(file.read(), Object.assign({
+			indent_size: 2
+		}, options||{}));
+		console.log(content);
+		return this.run('confirm', {
+			question: 'Do you like?'
+		}).then(function(a) {
+			if (a) {
+				file.write(content);
+				return true;
+			} else {
+				return null;
+			}
+		});
+	}
+}
+
+function validate(filename, options) {
+	return function() {
+		var file = this.touch(filename);
+		if (!filename||!file.exists()) {
+			this.warn('No file found');
+			return null;
+		} else {
+			return dronJsbeautifier(file, keysToBottomDashes(options));
+		}
+	}
+}
+
+module.exports = function factory(argv) {
+	var fileName = "string"===typeof argv._[1] ? argv._[1] : (argv.file || false);
+	return validate(fileName, argv);
+}
